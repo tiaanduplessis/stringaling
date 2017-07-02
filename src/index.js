6815044cdb9265512fc244779a1554a4c43ef6a7
Tiaan <tiaanduplessis@hotmail.com>
2017-07-02 22:00:47 +0200

    Add additional exports

--- src/index.js
+++ src/index.js
@@ -1,25 +1,35 @@
 import afterFirst from './after-first'
 import afterLast from './after-last'
 import append from './append'
+import around from './around'
 import at from './at'
+import beforeFirst from './before-first'
+import beforeLast from './before-last'
+import camelize from './camelize'
 import chars from './chars'
 import first from './first'
 import last from './last'
 import slugify from './slugify'
+import split from './split'
+import toTitleCase from './to-title-case'
 import truncate from './truncate'
-import around from './around'
 
 const helpers = {
   afterFirst,
   afterLast,
   append,
+  around,
   at,
+  beforeFirst,
+  beforeLast,
+  camelize,
   chars,
   first,
   last,
   slugify,
-  truncate,
-  around
+  split,
+  toTitleCase,
+  truncate
 }
 
 export default helpers
